0379a8fd635a812a3eac041987a7188740eb5d86
Andre Saddler <dresadd09691@gmail.com>
2020-11-09 23:14:07 -0500

    Basic URL shortner

---
+++ index.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const {
+    nanoid
+} = require('nanoid');
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGO);
+const urls = db.get('urls');
+urls.createIndex({
+    slug: 1
+}, {
+    unique: true
+});
+
+const app = express();
+
+//app.use(helmet());
+//app.use(helmet.contentSecurityPolicy());
+
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.get('/:id', async (req, res) => {
+    const {
+        id: slug
+    } = req.params;
+    try {
+        const url = await urls.findOne({
+            slug
+        });
+        if (url) {
+            urls.update({
+                    _id: url._id
+                }, {
+                    $set: {
+                        clicks: url.clicks + 1
+                    }
+                },
+                (err, result) => {}
+            )
+            res.redirect(url.url);
+        }
+        res.redirect(`/?error=${slug} not found`);
+    } catch (error) {
+        res.redirect(`/?error=Link not found`);
+    }
+});
+
+const urlSchema = yup.object().shape({
+    url: yup.string().trim().url().required(),
+});
+
+const slugSchema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+});
+
+app.post('/url', async (req, res, next) => {
+    let {
+        slug,
+        url
+    } = req.body;
+    try {
+        await urlSchema.validate({
+            url
+        });
+
+        if (!slug || slug == '') {
+            slug = nanoid(5);
+        }
+        slug = slug.toLowerCase();
+        await slugSchema.validate({
+            slug
+        });
+        const newUrl = {
+            url,
+            slug,
+            clicks: 0
+        }
+        const createdUrl = await urls.insert(newUrl);
+        delete createdUrl._id;
+        res.json(createdUrl);
+    } catch (error) {
+        if (error.message.startsWith('E11000')) {
+            error.message = 'Slug in use.'
+            error.status = 555;
+        }
+        next(error);
+    }
+});
+
+app.use((error, req, res, next) => {
+    if (error.status)
+        res.status(error.status);
+    else
+        res.status(500);
+
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? 'e' : error.stack,
+    });
+})
+
+const port = process.env.PORT || 7777;
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+})
---
+++ public/app.js
@@ -0,0 +1,60 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        url: '',
+        slug: '',
+        created: null,
+        useCustomSlug: false,
+        createdUrl: null,
+        error: null,
+    },
+    methods: {
+        async createUrl() {
+            this.createdUrl = null;
+            this.error = null;
+            this.created= null;
+
+            if (!this.useCustomSlug) {
+                this.slug = '';
+            }
+
+            if (this.useCustomSlug && this.slug == '') {
+                this.useCustomSlug = false;
+            }
+            
+            fetch('/url', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    url: this.url,
+                    slug: this.slug
+                })
+            })
+            .then((response) => {
+                //console.log(response);
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error(response.status);
+                }
+            })
+            .then (data => {
+                //console.log(data);
+                this.created = data;
+                this.createdUrl = location.protocol + '//' + location.host + '/' + this.created.slug;
+                console.log(this.createdUrl);
+            })
+            .catch (error => {
+                //console.log(error.message);
+                if (error.message == '555') {
+                    // console.log('Slug in use');
+                    this.error = "phrase already used";
+                } else {
+                    this.error = "something went wrong\nenter a full URL with http:// or https://";
+                }
+            });
+        }
+    }
+});
